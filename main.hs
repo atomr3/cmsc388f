299ba2f817acf22f487c76d4743d5cb538d34224
Anand Raghu <Anand@wireless-10-105-6-126.umd.edu>
2019-03-29 14:48:34 -0400

    initialize haskell file with starter code

---
+++ main.hs
@@ -0,0 +1,17 @@
+{-
+   Honor Pledge
+
+   I pledge on my honor that I have not given or received any
+   unauthorized assistance on this assignment.
+
+   Anand Raghu
+   Nelson Le
+   Andrew Witten
+
+   Module      :  <File name or $Header$ to be replaced automatically>
+   Description :  Based on information taken from `http://www.cs.cmu.edu/~rwh/theses/okasaki.pdf`
+-} 
+module Main where
+import Data.Monoid
+
+main = putStrLn "Hello World"
